c265ba9b541e9d1bf6cb99c76f09817915414379
Abiola Ibrahim <abiola.ibrahim@deimos.co.za>
2022-10-06 10:54:44 +0400

    add a basic loop to keep running

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println()
+	for {
+		time.Sleep(time.Second * 3)
+		fmt.Printf("the time in UTC is now %v", time.Now().UTC())
+		fmt.Println()
+	}
 }
