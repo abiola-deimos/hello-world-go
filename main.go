c6427ea3b4545dc58659509044fd83c409958fa2
Abiola Ibrahim <abiola.ibrahim@deimos.co.za>
2023-03-08 21:50:56 +0100

    add webapp and log levels

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+	"os"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println()
-	for {
-		time.Sleep(time.Second * 3)
-		fmt.Printf("the time in UTC is now %v", time.Now().UTC())
-		fmt.Println()
-	}
+	log.Debugf("application is starting and listening on 8080")
+
+	log.Debug("starting server")
+
+	// testing log outputs
+	log.Trace("testing trace log output")
+	log.Warn("testing warning log output")
+	log.Error("testing error log output")
+
+	log.Error(http.ListenAndServe(":8080", http.HandlerFunc(
+
+		func(w http.ResponseWriter, r *http.Request) {
+			log.Infof("%s, %+v, %s", r.URL.Path, r.Header, r.RemoteAddr)
+
+			hostname, _ := os.Hostname()
+			json.NewEncoder(w).Encode(map[string]any{
+				"current time in UTC": time.Now().UTC(),
+				"hostname is":         hostname,
+			})
+		},
+	)))
 }
